97e0003a977dfa40dd97042b9825e625f763675a
Prasant Shrestha <zeno7291@gmail.com>
2026-02-27 11:24:49 +0545

    added comments

--- server.py
+++ server.py
@@ -2,11 +2,13 @@ import socket
 import threading
 import json
 
+#Server Address (LocalHost)
 HOST = '127.0.0.1'
 PORT = 8000
 
 clients = {}  
 
+#Sends a message to all the clients except for the sender
 def broadcast(message, sender_socket):
     for client in clients:
         if client != sender_socket:
@@ -16,12 +18,14 @@ def broadcast(message, sender_socket):
                 client.close()
                 del clients[client]
 
+#Handle communication with one client
 def handle_client(client_socket):
     try:
         pubkey_data = client_socket.recv(4096).decode()
         clients[client_socket] = {'public_key': pubkey_data, 'addr': client_socket.getpeername()}
         print(f"Received public key from {clients[client_socket]['addr']}")
 
+        # Send the list of all public keys to every client
         def send_keys_update():
             keys = {str(addr): info['public_key'] for client, info in clients.items() for addr in [info['addr']]}
             keys_json = json.dumps(keys).encode()
@@ -30,6 +34,7 @@ def handle_client(client_socket):
 
         send_keys_update()
 
+        # Receive and forward messages
         while True:
             message = client_socket.recv(8192)
             if not message:
@@ -39,24 +44,30 @@ def handle_client(client_socket):
     except Exception as e:
         print(f"Error: {e}")
 
+     # When the client disconnects
     finally:
         print(f"Connection closed {clients[client_socket]['addr']}")
         client_socket.close()
+
+         # Remove the client from the dictionary
         if client_socket in clients:
             del clients[client_socket]
         send_keys_update()
 
+# Start the TCP server
 def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((HOST, PORT))
     server.listen()
     print(f"Server started on {HOST}:{PORT}")
 
+      # Accept clients forever
     while True:
         client_socket, addr = server.accept()
         print(f"New connection from {addr}")
         thread = threading.Thread(target=handle_client, args=(client_socket,))
         thread.start()
 
+# Entry point
 if __name__ == "__main__":
     start_server()
